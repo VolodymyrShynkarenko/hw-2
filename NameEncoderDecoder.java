1592a87c2578c83c210958e12e7ac26dc94e254a
volodymyr shykardos <51193222+VolodymyrShynkarenko@users.noreply.github.com>
2022-04-17 21:38:04 +0300

    Create NameEncoderDecoder.java

---
+++ NameEncoderDecoder.java
@@ -0,0 +1,25 @@
+public class NameEncoderDecoder {
+    public String encode(String name){
+        name = name
+                .replace("e","1")
+                .replace("u","2")
+                .replace("i","3")
+                .replace("o","4")
+                .replace("a","5");
+        name ="NOTFORYOU"+name+"YESNOTFORYOU";
+        return name;
+        }
+
+public String decode(String name) {
+        name = name
+        .replace("NOTFORYOU", "")
+        .replace("1", "e")
+        .replace("2", "u")
+        .replace("3", "i")
+        .replace("4", "o")
+        .replace("5", "a")
+        .replace("YES", "");
+        name ="" + name + "";
+        return name;
+        }
+        }
